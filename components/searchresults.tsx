5bc8a46f466a5740e8d79d0374d7d311fffefe6e
Rahul190299 <rahulgheware123@gmail.com>
2024-12-24 22:56:40 +0530

    improved ui

---
+++ components/searchresults.tsx
@@ -0,0 +1,29 @@
+
+
+
+export const SearchResults = () => {
+    return (
+        <div>
+            <div className="flex">
+                <div>
+                    <span>{} Results</span>
+                </div>
+                <div className="flex">
+                   <button >
+                    New
+                   </button>
+                   <button>
+                    Old
+                   </button>
+                   <button>
+                    Relevant
+                   </button>
+                </div>
+
+            </div>
+            <div className="">
+
+            </div>
+        </div>
+    )
+}
