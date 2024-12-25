1bb8462dea370dad555c76509efd5a88dd61e948
Rahul190299 <rahulgheware123@gmail.com>
2024-12-25 23:41:11 +0530

    added spinner component

---
+++ components/ui/spinner.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Spinner() {
+  return (
+    <div className="flex justify-center items-center">
+      <div className=" rounded-full h-10 w-10 border-t-2 border-b-2 border-gray-900"></div>
+    </div>
+  );
+};
+
+export default Spinner;
