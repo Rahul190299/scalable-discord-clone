b8232c967b1d26b8709cc61c951367b09be82317
Rahul190299 <rahulgheware123@gmail.com>
2024-12-25 23:40:45 +0530

    added ui components

--- app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -1,12 +1,12 @@
-import { FC } from 'react';
+import { FC } from "react";
 
-import { currentProfile } from '@/lib/current-profile';
-import { db } from '@/lib/db';
-import { redirect } from 'next/navigation';
-import ChatHeader from '@/components/chat/chat-header';
-import ChatInput from '@/components/chat/chat-input';
-import ChatMessages from '@/components/chat/chat-messages';
-
+import { currentProfile } from "@/lib/current-profile";
+import { db } from "@/lib/db";
+import { redirect } from "next/navigation";
+import ChatHeader from "@/components/chat/chat-header";
+import ChatInput from "@/components/chat/chat-input";
+import ChatMessages from "@/components/chat/chat-messages";
+import { SearchResults } from "@/components/searchresults";
 interface ChannelIdPageProps {
   params: {
     serverId: string;
@@ -17,7 +17,7 @@ interface ChannelIdPageProps {
 const ChannelIdPage: FC<ChannelIdPageProps> = async ({ params }) => {
   const profile = await currentProfile();
 
-  if (!profile) return redirect('/sign-in');
+  if (!profile) return redirect("/sign-in");
 
   const channel = await db.channel.findUnique({
     where: {
@@ -35,29 +35,36 @@ const ChannelIdPage: FC<ChannelIdPageProps> = async ({ params }) => {
   if (!channel || !member) redirect(`/`);
 
   return (
-    <div className="bg-white dark:bg-[#313338] flex flex-col h-full">
-      <ChatHeader
-        name={channel.name}
-        serverId={channel.serverId}
-        type="channel"
-      />
-      <ChatMessages
-        member={member}
-        name={channel.name}
-        chatId={channel.id}
-        type="channel"
-        apiUrl="/api/messages"
-        socketUrl="/api/socket/messages"
-        socketQuery={{ channelId: channel.id, serverId: channel.serverId }}
-        paramKey="channelId"
-        paramValue={channel.id}
-      />
-      <ChatInput
-        apiUrl="/api/socket/messages"
-        name={channel.name}
-        type="channel"
-        query={{ channelId: channel.id, serverId: channel.serverId }}
-      />
+    <div className="flex h-full">
+      <div className="basis-2/6 h-10 w-10">
+        <SearchResults/>
+      </div>
+      <div className="basis-4/6">
+        <div className="bg-white dark:bg-[#313338] flex flex-col h-full">
+          <ChatHeader
+            name={channel.name}
+            serverId={channel.serverId}
+            type="channel"
+          />
+          <ChatMessages
+            member={member}
+            name={channel.name}
+            chatId={channel.id}
+            type="channel"
+            apiUrl="/api/messages"
+            socketUrl="/api/socket/messages"
+            socketQuery={{ channelId: channel.id, serverId: channel.serverId }}
+            paramKey="channelId"
+            paramValue={channel.id}
+          />
+          <ChatInput
+            apiUrl="/api/socket/messages"
+            name={channel.name}
+            type="channel"
+            query={{ channelId: channel.id, serverId: channel.serverId }}
+          />
+        </div>
+      </div>
     </div>
   );
 };
