b480607bbe8b18a7fe2d114fb170572f9ad5a731
Rahul190299 <rahulgheware123@gmail.com>
2024-12-26 22:45:57 +0530

    added spinner

--- components/ui/spinner.tsx
+++ components/ui/spinner.tsx
@@ -1,11 +1,45 @@
 import React from 'react';
+import { cn } from '@/lib/utils';
+import { VariantProps, cva } from 'class-variance-authority';
+import { Loader2 } from 'lucide-react';
 
-function Spinner() {
+const spinnerVariants = cva('flex-col items-center justify-center', {
+  variants: {
+    show: {
+      true: 'flex',
+      false: 'hidden',
+    },
+  },
+  defaultVariants: {
+    show: true,
+  },
+});
+
+const loaderVariants = cva('animate-spin text-primary', {
+  variants: {
+    size: {
+      small: 'size-6',
+      medium: 'size-8',
+      large: 'size-12',
+    },
+  },
+  defaultVariants: {
+    size: 'medium',
+  },
+});
+
+interface SpinnerContentProps
+  extends VariantProps<typeof spinnerVariants>,
+    VariantProps<typeof loaderVariants> {
+  className?: string;
+  children?: React.ReactNode;
+}
+
+export default function Spinner({ size, show, children, className }: SpinnerContentProps) {
   return (
-    <div className="flex justify-center items-center">
-      <div className=" rounded-full h-10 w-10 border-t-2 border-b-2 border-gray-900"></div>
-    </div>
+    <span className={spinnerVariants({ show })}>
+      <Loader2 className={cn(loaderVariants({ size }), className)} />
+      {children}
+    </span>
   );
-};
-
-export default Spinner;
+}
