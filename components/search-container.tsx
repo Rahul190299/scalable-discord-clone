e3acfd08879c62a0c69d03623e080b99ed42cf03
Rahul190299 <rahulgheware123@gmail.com>
2025-01-02 23:35:43 +0530

    bug fixes

--- components/search-container.tsx
+++ components/search-container.tsx
@@ -1,14 +1,15 @@
-'use client'
+"use client";
 import { useState } from "react";
 import { SearchResults } from "./searchresults";
+import { useSearchStore } from "@/store/searchstore";
 
-
-export  const SearchContainer = () => {
-    const [basis,setBasis] = useState('basis-auto');
-    
-    return (
-        <div className={`${basis}`}>
-         <SearchResults />
+export const SearchContainer = () => {
+  const { showSearchPane } = useSearchStore();
+  return (
+    showSearchPane && (
+      <div className="basis-2/5">
+        <SearchResults />
       </div>
-    ) 
-}
+    )
+  );
+};
