358f86f60fdadf4aaf409f6fdf7b92945db6efb9
Rahul190299 <rahulgheware123@gmail.com>
2025-01-09 23:29:46 +0530

    added hook for seach messages

---
+++ hooks/use-chat-search.ts
@@ -0,0 +1,37 @@
+import qs from "query-string";
+import { useQuery } from "@tanstack/react-query";
+
+interface ChatQueryProps {
+  queryKey: string;
+  apiUrl: string;
+  paramKey: "channelId";
+  channelId: string;
+}
+
+export const useChatSearch = ({
+  apiUrl,
+  paramKey,
+  channelId,
+  queryKey,
+}: ChatQueryProps) => {
+  const searchMessages = async (queryKey : string) => {
+    const url = qs.stringifyUrl(
+      {
+        url: apiUrl,
+        query: {
+          page: queryKey,
+          [paramKey]: channelId,
+        },
+      },
+      { skipNull: true }
+    );
+    const res = await fetch(url);
+    return res.json();
+  };
+
+  const { status, error, data } = useQuery({
+    queryKey: [queryKey],
+    queryFn: ({queryKey}) =>  searchMessages(queryKey[0]),
+    staleTime : 500,
+  })
+};
