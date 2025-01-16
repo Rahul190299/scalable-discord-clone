98c1a45194aa4190410c9981b99e2d95018da66a
Rahul190299 <rahulgheware123@gmail.com>
2025-01-16 23:10:39 +0530

    bug fixes

--- components/search-container.tsx
+++ components/search-container.tsx
@@ -2,13 +2,29 @@
 import { useState } from "react";
 import { SearchResults } from "./searchresults";
 import { useSearchStore } from "@/store/searchstore";
+import { Member, Message, Profile } from '@prisma/client';
+import { ElementRef, FC, Fragment, useRef } from 'react';
 
-export const SearchContainer = () => {
+
+interface SearchMessagesProps {
+  member: Member;
+  chatId: string;
+  apiUrl: string;
+  paramKey: 'channelId';
+  paramValue: string;
+}
+export const SearchContainer : FC<SearchMessagesProps> = ({
+  apiUrl,
+  chatId,
+  member,
+  paramKey,
+  paramValue,
+}) => {
   const { showSearchPane } = useSearchStore();
   return (
     showSearchPane && (
       <div className="basis-2/5">
-        <SearchResults />
+        <SearchResults  />
       </div>
     )
   );
